9cfb2dd56924736247c96c32dc8dfc85aa28e39f
Darryl Kilzer <darrylkilzer@gmail.com>
2019-05-23 11:36:17 -0600

    oops

--- test/runner.js
+++ test/runner.js
@@ -1,9 +1,12 @@
 import './setup.js'
 import './modules/strings.test.js'
 import './modules/numbers.test.js'
-import './modules/objects.test.js'
-import './modules/arrays.test.js'
-import './modules/real-world-application.test.js'
+
+let loadDynamicModules = async function(){
+  await import("https://bcw.blob.core.windows.net/public/tester/.test/modules/dynamic.test.js")
+  mocha.run()
+}
 
 
-mocha.run()
+loadDynamicModules()
+
